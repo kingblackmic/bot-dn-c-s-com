a8d827fa7d3e002e065ef0640910c168572daea8
BlackMic100 <37043307+BlackMic100@users.noreply.github.com>
2018-04-11 15:10:14 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -317,4 +317,21 @@ client.on('message', message => {
     }
 });
 
+client.on('message', message => {
+    if (message.content.startsWith("*avatar")) {
+        var mentionned = message.mentions.users.first();
+    var x5bzm;
+      if(mentionned){
+          var x5bzm = mentionned;
+      } else {
+          var x5bzm = message.author;
+          
+      }
+        const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setImage(`${x5bzm.avatarURL}`)
+      message.channel.sendEmbed(embed);
+    }
+});
+
 client.login('NDMzNTc1MzI3MTk4MDg1MTIw.Da92Ew.KFNR9bmByCLDmO3iXem_5a2-9xc');
